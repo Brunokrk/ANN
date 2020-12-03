91e7e2277bb16edac409f639dd203cfcf5e38412
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-12-03 18:54:35 -0300

    .

--- metodo_bissecao/bisection.c
+++ metodo_bissecao/bisection.c
@@ -1,39 +1,50 @@
 #include <stdio.h>
 #include <math.h>
 
-void bisection(double(*f)(double), double a, double b, int n){
-        if(f(a) *f(b)<0){
-            double m =0;
-            for(int i=0; i<n; i++){
-                m=(a+b)/2;
-                if(f(m)==0){
-                    printf("A raiz procurada é %.16f\n", m);
-                    return;
-                }
-                printf("x_%d = %.16f\n", i+1, m);
-                if(f(a)*f(m)<0){
-                    b=m;
-                }else{
-                    a=m;
-                }
+void bisection(double (*f)(double), double a, double b, int n)
+{
+    if (f(a) * f(b) < 0)
+    {
+        double m = 0;
+        for (int i = 0; i < n; i++)
+        {
+            m = (a + b) / 2;
+            if (f(m) == 0)
+            {
+                printf("A raiz procurada é %.16f\n", m);
+                return;
             }
-        }else{
-            printf("%s\n", "O teorema de bolzano nao sabe dizer se existe raiz pra função neste intervalo");
+            printf("x_%d = %.16f\n", i + 1, m);
+            if (f(a) * f(m) < 0)
+            {
+                b = m;
+            }
+            else
+            {
+                a = m;
+            }
         }
+    }
+    else
+    {
+        printf("%s\n", "O teorema de bolzano nao sabe dizer se existe raiz pra função neste intervalo");
+    }
 }
 
-int main(){
+int main()
+{
     int max_iter = 10;
-    double a=-0.98881;
-    double b=3.04895;
+    double a = 0.94527;
+    double b = 1.98233;
 
-    double f (double x){
-        return (x*x*x*x - 2*x*x*x - 3*x*x +3*x + 2);
+    double f(double x)
+    {
+        return (3.14159265359*x - pow(2.7182818384590452353602874, x));
     }
 
-
-    double population(double lambda){
-        return 1000000*exp(lambda)+ (537142 /lambda) *(exp(lambda) - 1) -1863961;
+    double population(double lambda)
+    {
+        return 1000000 * exp(lambda) + (537142 / lambda) * (exp(lambda) - 1) - 1863961;
     }
 
     //bisection(f, a, b, max_iter);
