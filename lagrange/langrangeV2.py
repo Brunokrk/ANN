295cb5d798c0b02486356ebb97891e24691fd41a
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2021-03-25 13:11:21 -0300

    .

--- lagrange/langrangeV2.py
+++ lagrange/langrangeV2.py
@@ -7,7 +7,7 @@ def lagrange (x, y):
             numer = '*'.join([f'(x{-xi:+})' for i, xi in enumerate(x) if i!=k])
             denom = '*'.join([f'({x[k]}{-xi:+})' for i, xi in enumerate(x) if i!=k])
             eq += f'{y[k]:+}*({numer})/({denom})'
-            return eq
+        return eq
     else:
         raise TypeError('O numero de coordenadas X é diferente do número de coordenadas Y')
 
@@ -15,8 +15,8 @@ def lagrange (x, y):
 
 
 if __name__ == '__main__':
-    x = [1, 2, 3]
-    y = [2, 5, 1]
+    x = [1, 2, 3, 3.2, 4.1, 5.7]
+    y = [2, 5, 1, 2, 1, 10]
 
     eq = lagrange(x, y)
     
@@ -24,6 +24,22 @@ if __name__ == '__main__':
         return eval(eq)
 
     print('p(x) =', eq)
-    print(subs(1), subs(2), subs(3))
+    #print(subs(0.025), subs(4.648), subs(5.13))
 
+    #def f(x):
+        #return 1/(1+25*x**2)
     
+    #num = 5
+    #x2 = [-1 + (2/(num -1)) * i for i  in range(num)]
+    #y2 = [f(i) for i in x2]
+    #eq2 = lagrange(x2, y2)
+    #print(x2)
+    #print(y2)
+
+    from matplotlib import pyplot as plt
+    import numpy as np
+
+    t = np.linspace(1, 5.7, 100)
+    plt.plot(t, subs(t))
+    plt.scatter(x,y)
+    plt.savefig('lagrange.png')
